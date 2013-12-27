8f0153d52a5c7fbced3ff5cff1c848c77e3f029e
kicks66 <kicks66@gmail.com>
2013-12-27 21:09:16 +0000

    fixed bugs and menus

--- pins.js
+++ pins.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     $.getJSON('pins.php',function(data) {
     	var like = '';
     	for (var i=0 ; i<data.length ; i++) {
-    		like += "<div class='row'><div class='col-md-3'><center><img src='" + data[i]['Image'] + "' class='img-responsive img-circle'></center></div>";
-    		like += "<div class='col-md-8'><left><h3>" + data[i]['Event'] + "</h3></left></div></div>";
+    		like += "<div class='row'><div class='col-md-6'><center><img src='" + data[i]['Image'] + "' class='img-responsive img-thumbnail'></center></div>";
+    		like += "<div class='col-md-6'><left><h3>" + data[i]['Event'] + "</h3></left></div></div>";
     	}
     	$('#events').append(like);
     });
