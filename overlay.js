752092ced6e95bc4417afdd65e4dd2bd5e127cc6
kicks66 <kicks66@gmail.com>
2013-12-28 14:01:16 +0000

    remove buttons on mobile

--- overlay.js
+++ overlay.js
@@ -2,12 +2,12 @@ function hideDiv(){
 
     if ($(window).width() > 1024) {
 
-            $("#overlay").fadeOut("slow");
-
+            $("#overlay").fadeOut("fast");
     }else{
 
         $("#overlay").fadeIn("slow");
-
+        $('#swipeleftbtn_left').fadeOut("fast");
+        $('#swiperightbtn_right').fadeOut("fast");
     }
 
 }
