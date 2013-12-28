c7c8dace2debb9ba5ecbcdefe248e0c2553374d3
kicks66 <kicks66@gmail.com>
2013-12-28 14:16:47 +0000

    updated format of likes and pins page

--- pins.js
+++ pins.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
     $.getJSON('pins.php',function(data) {
     	var like = '';
     	for (var i=0 ; i<data.length ; i++) {
-    		like += "<div class='row'><div class='col-md-6'><center><img src='" + data[i]['Image'] + "' class='img-responsive img-thumbnail'></center></div>";
-    		like += "<div class='col-md-6'><left><h3>" + data[i]['Event'] + "</h3></left></div></div>";
+    		like += "<div class='row'>";
+    		like += "<div class='col-md-3'></div>";
+    		like += "<div class='col-md-3'><center><img src='" + data[i]['Image'] + "' class='img-pin img-responsive img-thumbnail'></center></div>";
+    		like += "<div class='col-md-3'><left><h3>" + data[i]['Event'] + "</h3></left></div>";
+    		like += "<div class='col-md-3'></div></div>";
     	}
     	$('#events').append(like);
     });
